1e813265877346d342c497d410bfbd73c789ce47
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-07 21:38:29 +0600

    new file has been added

---
+++ A. Operator/supermarket .js
@@ -0,0 +1,11 @@
+// Task-1
+
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// The first line of the input is the taka you have.
+// The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+//  result
+let taka = 1000;
+let 
+
