521b162facbe97d6f91ac2b8bd94814b9378dd5c
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-08 16:06:13 +0600

    added new problem and made it solved by mine

--- C. Array/check-array-books.js
+++ C. Array/check-array-books.js
@@ -1,6 +1,6 @@
 /**  problem-12
 Create an array of books containing different book.
-Use the includes method to check if the array contains a Javascript book.
-Print a message to the console indicating whether the element is present in the array or not. **/
+check if the array contains a Javascript book.
+Print a message indicating whether the element is present in the array or not. **/
 let books = ['Islamic', 'History', 'Philosophy', 'Politics', 'Novel'];
 console.log(books.includes('Javascript book'));
--- C. Array/isarray.js
+++ C. Array/isarray.js
@@ -1,7 +1,7 @@
 /**  problem-13
 Create different variables, each containing either an array or a non-array value.
-Now use isArray to check if each variable is an array.
-Print a message to the console indicating whether each variable is an array or not. **/
+check is it Array or not check if each variable is an array.
+Print a message indicating whether each variable is an array or not. **/
 let home = ['Normal Home', 'Simple Building', 'Dynamic Building', 'Expensive Home'];
 let person = NaN;
 let place = '';
