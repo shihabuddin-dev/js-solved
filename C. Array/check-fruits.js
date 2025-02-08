d5cbf8049dd59260f3e9d21249701bf0b9083484
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-08 15:47:31 +0600

    new file has been added

---
+++ C. Array/check-fruits.js
@@ -0,0 +1,9 @@
+/**  problem-11
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to Cherry
+console log the final array **/
+
+let fruits = ['apple', 'banana', 'orange', 'guava', 'berry'];
+fruits[2] = 'cherry';
+console.log(fruits[3]);
---
+++ C. Array/concat.js
@@ -0,0 +1,10 @@
+/**  problem-14
+Create two arrays of your choice.
+Then combine the two arrays into a new array.
+Print both the original arrays and the combined array. **/
+let topBrand = ['Apple', 'Microsoft', 'Google', 'Samsung', 'Dell'];
+let normalBrand = ['Lenovo', 'Xiaomi', 'Asus', 'One', 'Lava'];
+// combineBrand = topBrand.concat(normalBrand);
+console.log(topBrand);
+console.log(normalBrand);
+console.log(topBrand.concat(normalBrand));
