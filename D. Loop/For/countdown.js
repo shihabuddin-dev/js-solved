f420492933910d0a51855cc634e559fb3bc2dd85
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-09 06:15:39 +0600

    updated and added solution

--- D. Loop/For/countdown.js
+++ D. Loop/For/countdown.js
@@ -3,6 +3,16 @@
 Implement a countdown timer that counts down from 81 to 65.
 
  */
+
+// Solution-1
 for (let i = 81; i >= 65; i--) {
-    console.log(i);
+    if (i % 2 === 0 || i % 2 === 1) {
+        console.log(i);
+    }
+}
+
+
+// Solution-2 
+for (let j = 81; j >= 65; j--) {
+    console.log(j);
 }
