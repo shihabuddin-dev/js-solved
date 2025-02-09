ab022ae208b8aa86b891a816909e8d1d1a6dd182
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-09 17:49:16 +0600

    added new break problem solved

---
+++ D. Loop/Break/century.js
@@ -0,0 +1,11 @@
+/* Problem-1
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+*/
+// Solution-2 by for loop 
+for(let j=1; j<=200; j++){
+    if(j<=100){
+        break;
+    }
+    console.log(j);
+}
