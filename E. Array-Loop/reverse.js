59bfcc9045f56a591a87dc599579e8f3fdee920f
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-09 18:53:37 +0600

    new file has been added here

---
+++ E. Array-Loop/reverse.js
@@ -0,0 +1,9 @@
+/** Write a JavaScript code to reverse the array colors without using the reverse method.
+
+Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+Output:
+['orange', 'yellow', 'green', 'blue', 'red'] **/
+
+let colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+console.log(colors.reverse);
